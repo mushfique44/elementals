75dc78c2420af7117c5379e012f34bbd9d49bd78
mushfique44 <mushfique44@gmail.com>
2024-02-23 23:27:24 +0000

    feat JS: creating initial functions for Java script

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+function runGame() {
+
+}
+
+function playerSelection() {
+
+}
+
+function compRandSelection() {
+
+}
+
+function whoWinsRound() {
+
+}
+
+function playerWins() {
+
+}
+
+function draw() {
+
+}
+
+function compWins() {
+
+}
+
+function whoWins() {
+
+}
+
+function incrementRound() {
+
+}
+
+function incrementWins() {
+
+}
+
+function incrementLosses() {
+
+}
+
+function restartGame() {
+
+}
+
+function helpScreen() {
+
+}
