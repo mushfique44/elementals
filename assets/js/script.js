4acd34154674e48e761dcc85ff12f8886ddc0e4d
mushfique44 <mushfique44@gmail.com>
2024-02-24 00:24:19 +0000

    feat: adding an inital button click feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,25 @@
+// DOM loading
+// button elements and event listeners
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "electric") {
+            alert('You Clicked Electric element');
+            } else if (this.getAttribute("data-type") === "fire") {
+                alert('You Clicked Fire element');
+            } else if (this.getAttribute("data-type") === "water") {
+                alert('You Clicked Water element');
+            } else if (this.getAttribute("data-type") === "earth") {
+                alert('You Clicked Earth element');
+            }
+        })
+    }
+})
+
+
 function runGame() {
 
 }
