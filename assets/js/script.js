1c587bd4fe0450e2549577c582ccad4769f06a63
mushfique44 <mushfique44@gmail.com>
2024-02-26 22:45:39 +0000

    edit JS: commenting all the code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 
 //Declaring variables to equal elements from the HTML
-const electicButton = document.querySelector('.btn_electric');
 const playerSelectDisplay = document.querySelector('.player_element');
 const compSelectDisplay = document.querySelector('.comp_element');
 const roundNo = document.querySelector('.round');
@@ -12,9 +11,10 @@ const helpBtn = document.querySelector('.help_btn')
 const helpCloseBtn = document.querySelector('.help_close');
 const endGameDisplay = document.querySelector('.end_game');
 
-
+//Hidding the help and end game screens
 endGameDisplay.style.visibility = 'hidden';
 helpDisplay.style.visibility = 'hidden';
+//execute help screen open and close functions
 helpScreenOn();
 helpScreenOff();
 
@@ -51,7 +51,6 @@ const outcomes = [{
 },
 ];
 
-const noWins = 0;
 
 // DOM loading
 // button elements and event listeners
@@ -59,13 +58,13 @@ const noWins = 0;
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
     
+    // setting values of button clicked to elementChoice
     for (let button of buttons) {
         button.addEventListener("click", function() {
             let elementChoice = this.getAttribute("data-type");
-            console.log(elementChoice);
             runGame(elementChoice);
 
-
+            // Reload page when clicking the reload button
             if (this.getAttribute("data-type") === "reload"){
                 location.reload();
             }
@@ -78,42 +77,48 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-
+//Create run game function
 function runGame(playerSelects) {
+    // create variable to equal elements of compRandSelection function
     let compSelects = compRandSelection();
-    // console.log(compSelects['name']);
-    // console.log(compSelects['beats']);
 
+    // Rewrite what the player and computer picked onto the id elements on the HTML
     document.getElementById('last_pick').textContent = playerSelects;
     document.getElementById('comp_element').textContent = compSelects['name'];
     
     
-
-    const result = whoWinsRound(playerSelects, compSelects);
-    //alert(result);
+    // Result variable equal the winner of the round by running whoWinsRound function
+    let result = whoWinsRound(playerSelects, compSelects);
+    // Display the winner of the round to Id in HTML
     document.getElementById('round_winner').textContent = result;
-   
 
 }
 
+// function to set what element player selects
 function playerSelection(elementChoice) {
     let playerSelects = elementChoice;
 
 }
 
+// function to set what element computer randomly selects
 function compRandSelection() {
+    // Randomise a number 1-4 and give it appropriate array index from the outcomes array
     let randSelect = outcomes[Math.floor(Math.random()* outcomes.length)];
     return randSelect;
 }
 
 
-
+// function to run the winner of the round
 function whoWinsRound(playerSelects, compSelects) {
+    // increment the number of round played
     incrementRound();
+
+    // if statment to determine who wins the round
     if (playerSelects === compSelects['name']) {
         return "You Draw";
         
     } else if (playerSelects === compSelects['lossTo']) {
+        // increment player wins
         incrementWins();
         return "You Win!";
         
@@ -121,6 +126,7 @@ function whoWinsRound(playerSelects, compSelects) {
     } else if (playerSelects === compSelects['drawTo']){
         return "You Draw";
     } else {
+        // inrement computer wins
         incrementLosses();
         return "You Lose";
        
@@ -129,15 +135,14 @@ function whoWinsRound(playerSelects, compSelects) {
     
 }
 
-/**
- * Tallying of Rounds
- */
+// function to tallying of Rounds
 function incrementRound(prevWins) {
     //Declare variable to equal the value of the ID element 'round'
     let prevRound = parseInt(roundNo.innerText);
     //This will set the Inner text of 'round' ID to 1 + prevWins
     roundNo.innerText = ++prevRound;
     
+    // once 5 round is complete run gameEnd function and show who wins game
     if (prevRound === 5) {
         gameEnd();
         if (parseInt(incrementWins()-1) === parseInt(incrementLosses()-1)){
@@ -150,9 +155,7 @@ function incrementRound(prevWins) {
     }
 }
 
-/**
- * Tallying of Wins
- */
+// function to tally wins
 function incrementWins() {
     //Declare variable to equal the value of the ID element 'wins'
     let prevWins = parseInt(winNo.innerText);
@@ -162,9 +165,7 @@ function incrementWins() {
     
 }
 
-/**
- * Tallying of Losses
- */
+// function to tally losses
 function incrementLosses() {
     //Declare variable to equal the value of the ID element 'loss'
     let prevLoss = parseInt(lossNo.innerText);
@@ -174,14 +175,15 @@ function incrementLosses() {
     
 }
 
+// function to display the end game screen
 function gameEnd() {
     endGameDisplay.style.visibility = 'visible';
 }
 
 
-
+// function to display the help screen
 function helpScreenOn() {
-    
+    // Display screen when helpBtn is clicked
     helpBtn.addEventListener('click', function(){
         helpDisplay.style.visibility = 'visible'; 
           
@@ -190,8 +192,9 @@ function helpScreenOn() {
     
 }
 
+// function to close help screen
 function helpScreenOff() {
-    
+    // Hide help screen when helpCloseBtn is clicked
     helpCloseBtn.addEventListener('click', function(){
         helpDisplay.style.visibility = 'hidden'; 
         
